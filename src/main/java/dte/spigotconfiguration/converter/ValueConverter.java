c5ddb294fa7a84ed045eda9dff799be2756ec10e
David <69223217+DavidTheExplorer@users.noreply.github.com>
2022-11-04 22:26:41 +0200

    Added a builtin converter for Material

--- src/main/java/dte/spigotconfiguration/converter/ValueConverter.java
+++ src/main/java/dte/spigotconfiguration/converter/ValueConverter.java
@@ -1,9 +1,13 @@
 package dte.spigotconfiguration.converter;
 
+import org.bukkit.Material;
+
 import dte.spigotconfiguration.exceptions.ConfigLoadException;
 
 @FunctionalInterface
 public interface ValueConverter<T>
 {
 	T convertFrom(Object object) throws ConfigLoadException;
+	
+	ValueConverter<Material> MATERIAL = (materialName) -> Material.matchMaterial(materialName.toString().toUpperCase());
 }
